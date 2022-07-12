8ea235da58b151912996ac1c4386ec5858a6519a
Alexwebcreator <Sanek72Tob@mail.ru>
2022-07-12 14:53:51 +0200

    added count of try

--- script.js
+++ script.js
@@ -1,68 +1,44 @@
 'use strict';
 
-const start = 1, end = 100;
-
-function getRandomInRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const guesser = function(ans, num) {
-  
-  switch (true) {
-  case ans === null:
-    alert('Игра окончена');
-    return { gameover: true };
-  case isNaN(+ans):
-    return { msg: 'Введи число!', gameover: false };
-  case +ans > end || +ans < start:
-    return {
-      msg: `Число должно быть от ${start} до ${end}`,
-      gameover: false,
-    };
-  case +ans > num:
-    return {
-      msg: `Загаданное число меньше ${ans}.`,
-      gameover: false,
-    };
-  case +ans < num:
-    return {
-      msg: `Загаданное число больше ${ans}.`,
-      gameover: false,
-    };
-  case +ans === num:
-    return {
-      msg: `Поздравляю, Вы угадали!!!`,
-      gameover: true,
-    };
-  }
+const isNumber = function (value) {
+  return !isNaN(parseFloat(value)) && isFinite(value);
 };
 
-const game = secret => rules => {
-
-  const answer = prompt(`Угадай число от ${start} до ${end}?`);
-  const { msg, gameover } = rules(answer, secret);
-  let repeat = false;
-
-  if (gameover) {
-    if (msg === 'Игра окончена') {
-      repeat = false;
-    } else {
-      repeat = confirm(msg);
+const getNum  = () => {
+  const start = 1;
+  const end = 100;
+  let tryCount = 10;
+  let n = Math.floor(Math.random() * (end - start + 1)) + start;
+  const testNum = () => {
+    let ask = +prompt(`Угадай число от ${start} до ${end} У тебя осталось ${tryCount} попыток`);
+    tryCount--;
+    if (tryCount<=0) {
+      let gameover = confirm("Попытки закончились, хотите сыграть еще?");
+      if (gameover) {
+        getNum();
+      }
+    } else if (!isNumber(ask)) {
+      alert('Введите число!');
+      tryCount++;
+      testNum();
+    } else if (ask === null) {
+      alert('Игра окончена!');
+    } else if (+ask > n) {
+      alert(`Загаданное число меньше ${ask}. У вас осталось ${tryCount} попыток`);
+      testNum();
+    } else if (+ask < n) {
+      alert(`Загаданное число больше ${ask}. У вас осталось ${tryCount} попыток`);
+      testNum();
+    } else if (+ask === n) {
+      let ifWin = confirm('Поздравляем, Вы угадали!!! Хотели бы сыграть еще?');
+      if (ifWin) {
+        getNum();
+      } else {
+        alert('Спасибо за игру!!!');
+        getNum();
+      }
     }
-  } else {
-    alert(msg);
-    repeat = game(secret)(rules);
-  }
-
-  return repeat;
-
+  };
+  testNum();
 };
-
-const letsPlay = function() {
-  document.title = `Угадай число`;
-  const secret = getRandomInRange(start, end);
-  const playAgain = game(secret)(guesser);
-  if (playAgain) letsPlay();
-};
-
-letsPlay();
+getNum();
